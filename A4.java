219701f635a725d84e55d6ee952185aae07a0edc
tanmaysingh18 <53615354+tanmaysingh18@users.noreply.github.com>
2019-08-29 02:32:28 +0530

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,25 @@
+
+public class A4 {
+
+	public static void main(String[] args) {
+	int i,j,s=0,n,rw=0,r,q;
+	for(i=100;i<1000;i++);{
+	for(j=100;j<1000;j++);{
+	n=i*j;
+	q=n;
+	while(q>0)
+	{
+		r=q%10;
+		rw=rw*10+r;
+		q=q/10;}
+		if(rw==n&&n>s) {
+			s=rw;
+			rw=0;}
+			
+		}
+	}
+System.out.println(s);
+	
+	}
+
+}
