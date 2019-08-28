d358af834bf192c62b6b01043c9ed4f0fe62605e
tanmaysingh18 <53615354+tanmaysingh18@users.noreply.github.com>
2019-08-29 02:30:21 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,21 @@
+
+public class A1 {
+
+	public static void main(String[] args) {
+int i;
+long t=0;
+for(i=1;i<1000;i++)
+{
+	if((i%3==0)||(i%5==0))
+	{
+		t=t+i;
+		
+		System.out.println(t);
+		
+
+	}
+}
+
+}
+
+}
