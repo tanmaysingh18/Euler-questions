453fa95835b02179af6d8dab0c56b71472d6e8dc
tanmaysingh18 <53615354+tanmaysingh18@users.noreply.github.com>
2019-08-31 23:21:09 +0530

    Add files via upload

---
+++ A6.java
@@ -0,0 +1,14 @@
+
+public class A6 {
+
+	public static void main(String[] args) {
+	long n=100,m,a,s,p;
+	a=(n*(n+1)*(2*n+1))/6; //Sum of Squares of 100 natural numbers
+	m=(n*(n+1))/2;  //sum of firstb100 natural numbers
+	s=m*m;// square of sum of first 100 natural numbers
+	p=s-a;
+	System.out.println(p);
+return ;
+	}
+
+}
