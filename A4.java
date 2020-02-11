314c8924219ad5267e468757360dde9782580e8d
tanmaysingh18 <53615354+tanmaysingh18@users.noreply.github.com>
2020-02-11 21:48:36 +0530

    Update A4.java

--- A4.java
+++ A4.java
@@ -2,21 +2,20 @@
 public class A4 {
 
 	public static void main(String[] args) {
-	int i,j,s=0,n,rw=0,r,q;
+	long i,j,s=0,n,rw=0,r,q;
 	for(i=100;i<1000;i++){
 	for(j=100;j<1000;j++){
 	n=i*j;
-	q=n;
+	q=n;rw=0;
 	while(q>0)
 	{
 		r=q%10;
 		rw=rw*10+r;
 		q=q/10;}
-		if(rw==n&&n>s) {
+		if(rw==n&&n>s) 			
 			s=rw;
-			rw=0;}
 			
-		}
+	}
 	}
 System.out.println(s);
 	
